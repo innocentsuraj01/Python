5d15ce6dffcf478631c4adfea2d047cfeed37b65
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-02-05 09:53:25 +0530

    added a new line

--- evaluate.py
+++ evaluate.py
@@ -2,4 +2,4 @@ a = int(input("Enter a number:"))
 b = int(input("Enter b number:"))
 c = int(input("Enter c number:"))
 
-print ("Evaluate of three numbers:", a + (b*c))
+print ("Evaluate of three numbers:", a * (b+c))
