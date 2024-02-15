8156b459cfcbdd7ca1e8c04d9a67b293b48c9e98
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-02-15 23:49:21 +0530

    add a new line

--- pattern.py
+++ pattern.py
@@ -1,5 +1,15 @@
-r = int(input("Entern number of row:"))
+r = int(input("Enter number of row:"))
 for i in range (0, r):
- for j in range (0, i):
-    print ("*", end ="")
- print()
+   for j in range (0, r-i+1):
+      print (end=" ")
+   for j in range (0, i):
+      print ("*", end ="")
+   print()
+
+for i in range (r, 0,-1):
+   for j in range (0, r-i+1):
+      print (end=" ")
+   for j in range (0, i):
+      print ("*", end ="")
+   print()
+
