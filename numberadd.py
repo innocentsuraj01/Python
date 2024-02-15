2083e9a893ec28a6c606b49873ba285d7233540c
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-02-15 23:05:25 +0530

    Added anew line

--- numberadd.py
+++ numberadd.py
@@ -1,7 +1,8 @@
 n = int(input("Eneter a number:"))
+num = n
 Result = 0
 while n > 0: 
     digit = n % 10
     n = n // 10
     Result = Result + digit
-print ("sum of digit of", n ,"is", Result)
+print ("sum of digit of", num ,"is", Result)
