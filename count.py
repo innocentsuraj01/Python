e7eea05d132a2dc67934b5dc3159b50cc1618bf4
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-02-26 10:07:52 +0530

    added a new line

--- count.py
+++ count.py
@@ -1,3 +1,2 @@
 list1 = [10,20,30,40,50]
-list1 . count(10)
-print (list1)
+print(list1 . count(10))
--- index.py
+++ index.py
@@ -1,3 +1,3 @@
 list1 = [10,20,30,40,50,10,20]
-list1 . index(20)
-print (list1)
+print(list1 . index(20))
+
